2c58f6202d3b6beae5e1d706f64135bb7b62bdfe
bx-lr <41136019+bx-lr@users.noreply.github.com>
2022-06-16 17:17:32 -0500

    initial test env built with pure python ssdeep

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # dbfuzzhash
 Fuzzy hash search project using databases. 
+
+## Installation
+fysa: im using Python 3.8.2
+You might want to make a virtual environment first.....
+
+```cmd
+python -m venv .venv
+```
+
+Next, activate the virtual environment 
+```
+ .\.venv\Scripts\Activate.ps1
+```
+
+install dependencies
+```cmd
+python -m pip install -r requirements.txt
+```
+
+## Use
+run test.py
+
+
+## Deactivation 
+
+```cmd
+deactivate
+```
+
+# TODO
+possible dataset available at 
+https://www.nist.gov/itl/ssd/software-quality-group/ssdeep-datasets
---
+++ test.py
@@ -0,0 +1,32 @@
+import ppdeep
+import os
+from subprocess import Popen, PIPE
+def main():
+    print('hello....\nrunning test...')
+    working = os.getcwd()
+    os.chdir('hash_testing' + os.path.sep + 'samples')    
+    samples = ['lorem_01.txt', 'lorem_02.txt', 'lorem_03.txt']
+    h1 = ppdeep.hash_from_file(samples[0])
+    print('hash of %s is %s' % (samples[0], h1))
+    h2 = ppdeep.hash_from_file(samples[1])
+    print('hash of %s is %s' % (samples[1], h2))
+    h3 = ppdeep.hash_from_file(samples[2])
+    print('hash of %s is %s' % (samples[2], h3))
+    
+    val = ppdeep.compare(h1,h2)
+    print('%s to %s scores: %d' % (samples[0], samples[1], val))
+    
+    val = ppdeep.compare(h1,h3)
+    print('%s to %s scores: %d' % (samples[0], samples[2], val))
+    
+    val = ppdeep.compare(h2,h3)
+    print('%s to %s scores: %d' % (samples[1], samples[2], val))
+
+    os.chdir(working)
+    process = Popen(["hash_testing\\bins\\ssdeep-2.14.1\\ssdeep.exe", "-s", "hash_testing\\samples\\*.txt"], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    print('\nssdeep.exe results')
+    print(stdout.decode("utf-8"))
+    print('done')
+if __name__ == '__main__':
+    main()
